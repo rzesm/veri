cbfacb153ccfa5c101dae05f66ad1df49f86de9e
rzes <veirddd@gmail.com>
2026-05-25 20:42:35 +0200

    change no ui gaps

--- internal/filesystem/home/user/.config/hypr/scripts/toggle_ui.sh
+++ internal/filesystem/home/user/.config/hypr/scripts/toggle_ui.sh
@@ -1,9 +1,9 @@
 if pgrep -x waybar >/dev/null; then 
     pkill waybar
     # hyprctl keyword plugin:hyprbars:enabled false
-    hyprctl keyword general:gaps_out 6
-    hyprctl keyword general:gaps_in 3
-    hyprctl keyword decoration:rounding 10
+    hyprctl keyword general:gaps_out 2
+    hyprctl keyword general:gaps_in 1
+    hyprctl keyword decoration:rounding 8
 else
     waybar & disown
     # hyprctl keyword plugin:hyprbars:enabled true
